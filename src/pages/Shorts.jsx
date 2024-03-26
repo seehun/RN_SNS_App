0d2731c8ed4b8315ea0159b60e74d590d077f491
seehun <hwangbird99@gmail.com>
2024-03-26 15:41:27 +0900

    쇼츠 기본 구현

--- src/pages/Shorts.jsx
+++ src/pages/Shorts.jsx
@@ -1,14 +1,53 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  FlatList,
+  useWindowDimensions,
+} from 'react-native';
+import Video from 'react-native-video';
 import React from 'react';
 
+import dummy_video from '../static/dummy_video';
+
 const Shorts = () => {
+  const {width, height} = useWindowDimensions();
+  const renderVideo = ({item}) => {
+    return (
+      <View>
+        <Video
+          source={{uri: item.url}}
+          resizeMode="cover" //cover , contain
+          playInBackground={false} //background에서 동작 x
+          playWhenInactive={false}
+          repeat={true}
+          rate={1}
+          style={{width: width, height: height}}
+        />
+      </View>
+    );
+  };
   return (
-    <View>
-      <Text>Shorts</Text>
-    </View>
+    <SafeAreaView style={{flex: 1, backgroundColor: '#fff'}}>
+      <View style={styles.screen}>
+        <FlatList
+          data={dummy_video}
+          renderItem={renderVideo}
+          keyExtractor={item => item.id}
+          removeClippedSubviews
+          showsVerticalScrollIndicator={false}
+          snapToInterval={height} //얼마만큼 snap 될건지
+          snapToAlignment="start" //snap의 시작점
+          decelerationRate={'fast'} //빠르게 snap 하겠다
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default Shorts;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {flex: 1},
+});
---
+++ src/static/dummy_video.js
@@ -0,0 +1,44 @@
+const dummy_video = [
+  {
+    id: 1,
+    url: 'https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4',
+    user: 'asdf',
+    comment: '123hjk1l',
+    like: 123,
+    comments: 23,
+  },
+  {
+    id: 2,
+    url: 'https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4',
+    user: 'asdf',
+    comment: '123hjk1l',
+    like: 123,
+    comments: 23,
+  },
+  {
+    id: 3,
+    url: 'https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4',
+    user: 'asdf',
+    comment: '123hjk1l',
+    like: 123,
+    comments: 23,
+  },
+  {
+    id: 4,
+    url: 'https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4',
+    user: 'asdf',
+    comment: '123hjk1l',
+    like: 123,
+    comments: 23,
+  },
+  {
+    id: 5,
+    url: 'https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4',
+    user: 'asdf',
+    comment: '123hjk1l',
+    like: 123,
+    comments: 23,
+  },
+];
+
+export default dummy_video;
